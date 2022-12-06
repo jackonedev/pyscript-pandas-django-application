9fb3d4003e03c5833a5bf6d9cbdfa6ac6cb7d40d
AgustinSt1990 <jackone.action.software@gmail.com>
2022-12-06 07:23:48 -0300

    APRETANDO EL BOTON Y RECIBIENDO TODA LA CARD-BODY

--- static/script.py
+++ static/script.py
@@ -1,9 +1,8 @@
 from js import document, console
 import json
-import pprint
-import pyodide
+from pyodide import create_proxy
 from pyodide.http import pyfetch
-
+from itertools import count
 
 # def load_json_file(path):
 #     """ no funciona"""
@@ -54,42 +53,69 @@ def read_json():
     return json.loads(json_data)
 
 def fetchData(log=False):
-    console.log("\nIngresando a fetchData")
+    global x
+    console.log(f"\n\tlog_[{next(x)}]\nIngresando a fetchData")
     try:
         # response = load_json_file("./api.json")
         response = read_json()
         data = json.dumps(response, indent=2)
-        if log: console.log(data)
+        if log: console.log(f"\n\tlog_[{next(x)}]\n{data}")
         return response
     except Exception as error:
         console.log(error)
 
 def pintarCards(data, log=False):
-    console.log("\nIngresando a pintarCards")
+    global x
+    console.log(f"\n\tlog_[{next(x)}]\nIngresando a pintarCards")
     fragment = document.createDocumentFragment()
     for producto in data:
-        if log: console.log(str(producto))
+        if log: console.log(f"\n\tlog_[{next(x)}]\n{str(producto)}")
         ### IMPORTANTE
         templateCard.querySelector('h5').textContent = producto['title']
         templateCard.querySelector('p').textContent = f"${producto['precio']}"
         templateCard.querySelector('img').setAttribute('src', producto['thumbnailUrl'])
+        templateCard.querySelector('.btn-dark').dataset.id = producto['id']
+
         clone = templateCard.cloneNode(True)
         fragment.appendChild(clone)
         ###
-
-
     items.appendChild(fragment)
+
+def addCarrito(e, log=False):
+    global x
+    console.log(f"\n\tlog_[{next(x)}]\nIngresando a addCarrito")
+    if log: console.log(f"\n\tlog_[{next(x)}]");console.log(e.target)
+    
+    ### IMPORTANTE
+    if e.target.classList.contains('btn-dark'):
+        console.log(f"\n\tlog_[{next(x)}]");console.log(e.target.parentElement)
+    e.stopPropagation()
+
+class Carrito:
+    pass
+
+
+def setCarrito(objeto):
+    """modificar el objeto Carrito"""
+    pass
+
+
 
 
 def main():
     global templateCard, items
-    console.log("\nIngresando al main")
-    
+    global x
+    x = count(1)    
+    console.log(f"\n\tlog_[{next(x)}]\nIngresando al main")
+
+
     data = fetchData()
     templateCard = document.getElementById('template-card').content
     items = document.getElementById('items')
+    
+    pintarCards(data)
 
-    pintarCards(data)#TODO
+    items.addEventListener('click', create_proxy(addCarrito))
     # document.addEventListener("DOMContentLoaded", pyodide.create_proxy(fetchData))
 
 
